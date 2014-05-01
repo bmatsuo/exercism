be047cf5e7b61d4884d9ade3e37c64cf1606b1cd
Bryan Matsuo <bmatsuo@janrain.com>
2014-05-01 15:05:26 -0500

    second go exercise (raindrops)

---
+++ go/raindrops/raindrops.go
@@ -0,0 +1,34 @@
+package raindrops
+
+import (
+	"bytes"
+	"fmt"
+)
+
+// Convert translates integers into randrop-speak.  The randrop-speak
+// specification can be found at
+// http://exercism.io/exercises/go/raindrops/readme
+func Convert(x int) string {
+	var buf bytes.Buffer
+	var primeFound bool
+	for _, tr := range factorTranslations {
+		if x%tr.prime == 0 {
+			primeFound = true
+			buf.Write(tr.text)
+		}
+	}
+	if !primeFound {
+		fmt.Fprint(&buf, x)
+	}
+	return buf.String()
+}
+
+// raindrop-speak prime factor translations
+var factorTranslations = []struct {
+	prime int
+	text  []byte
+}{
+	{3, []byte("Pling")},
+	{5, []byte("Plang")},
+	{7, []byte("Plong")},
+}
