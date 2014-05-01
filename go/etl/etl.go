8aaba449c55ec249c0722ffd3f51901044449db3
Bryan Matsuo <bmatsuo@janrain.com>
2014-05-01 17:41:40 -0500

    godoc (etl)

--- go/etl/etl.go
+++ go/etl/etl.go
@@ -6,6 +6,9 @@ import "strings"
 // characters this will be wrong.  But its only used to reduce allocations.
 const alphabetSize = 26
 
+// Transform converts the legacy Scrabble point-value table format into a table
+// in the new format.  Strings in the legacy format will be converted lowercase
+// in the new format.
 func Transform(legacy map[int][]string) map[string]int {
 	m := make(map[string]int, alphabetSize)
 	for score := range legacy {
