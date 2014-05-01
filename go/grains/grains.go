cd110a05ae6d60b9eb6c701e276d55920031462f
Bryan Matsuo <bmatsuo@janrain.com>
2014-05-01 16:53:13 -0500

    third go exercise (grains)

---
+++ go/grains/grains.go
@@ -0,0 +1,22 @@
+package grains
+
+import "math"
+
+// Square returns the number of grains of wheat on square i of the chessboard.
+func Square(i int) uint64 {
+	if i < 0 {
+		panic("invalid square")
+	}
+	return uint64(1) << uint(i-1)
+}
+
+// TotalGrains is the number of grains on the chessboard.  The number of grains
+// on each square is a power of two (binary representation: 00...00100...00).
+// The sum of these 64 unique values is a 64 bit unsigned integer with all bits
+// set to one.
+const TotalGrains = math.MaxUint64
+
+// Total returns the number of grains on the chessboard. See TotalGrains.
+func Total() uint64 {
+	return TotalGrains
+}
