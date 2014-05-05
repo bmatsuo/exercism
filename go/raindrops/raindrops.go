3c8312483b7fe66a26f6015253dd1c3a8c30b1e6
Bryan Matsuo <bryan.matsuo@gmail.com>
2014-05-04 20:47:09 -0600

    clean up raindrops (go)

--- go/raindrops/raindrops.go
+++ go/raindrops/raindrops.go
@@ -1,7 +1,6 @@
 package raindrops
 
 import (
-	"bytes"
 	"strconv"
 )
 
@@ -9,26 +8,24 @@ import (
 // specification can be found at
 // http://exercism.io/exercises/go/raindrops/readme
 func Convert(x int) string {
-	var buf bytes.Buffer
-	var primeFound bool
+	var rain string
 	for _, tr := range factorTranslations {
 		if x%tr.prime == 0 {
-			primeFound = true
-			buf.Write(tr.text)
+			rain += tr.text
 		}
 	}
-	if !primeFound {
+	if rain == "" {
 		return strconv.Itoa(x)
 	}
-	return buf.String()
+	return rain
 }
 
 // raindrop-speak prime factor translations
 var factorTranslations = []struct {
 	prime int
-	text  []byte
+	text  string
 }{
-	{3, []byte("Pling")},
-	{5, []byte("Plang")},
-	{7, []byte("Plong")},
+	{3, "Pling"},
+	{5, "Plang"},
+	{7, "Plong"},
 }
